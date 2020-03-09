c510e5590dbce7828f9b6d77caa12eaedd99d195
余冬 <yudong@cvte.com>
2020-03-09 15:39:54 +0800

    上传图片

--- Kybs0.Net.Utils/Web_/WebFileUploadHelper.cs
+++ Kybs0.Net.Utils/Web_/WebFileUploadHelper.cs
@@ -13,6 +13,9 @@ namespace Kybs0.Net.Utils
     public class WebFileUploadHelper
     {
         private static readonly Encoding DefaultEncode = Encoding.UTF8;
+
+        #region UplodFile
+
         /// <summary>
         /// HttpUploadFile
         /// </summary>
@@ -21,7 +24,7 @@ namespace Kybs0.Net.Utils
         /// <returns></returns>
         public static string UploadFile(string url, string file)
         {
-            return UploadFile(url, file, new NameValueCollection(), DefaultEncode);
+            return UploadFile(url, file, null, DefaultEncode);
         }
         /// <summary>
         /// HttpUploadFile
@@ -45,19 +48,18 @@ namespace Kybs0.Net.Utils
         /// <returns></returns>
         public static string UploadFile(string url, string file, NameValueCollection data, Encoding encoding)
         {
-            return UploadFile(url, new string[] { file }, data, encoding);
+            return UploadFiles(url, new string[] { file }, data, encoding);
         }
-
         /// <summary>
         /// HttpUploadFile
         /// </summary>
         /// <param name="url"></param>
-        /// <param name="files"></param>
+        /// <param name="file"></param>
         /// <param name="data"></param>
         /// <returns></returns>
-        public static string UploadFile(string url, string[] files, NameValueCollection data)
+        public static string UploadFiles(string url, string file, NameValueCollection data)
         {
-            return UploadFile(url, files, data, DefaultEncode);
+            return UploadFiles(url, new string[] { file }, data, DefaultEncode);
         }
 
         /// <summary>
@@ -68,15 +70,124 @@ namespace Kybs0.Net.Utils
         /// <param name="data"></param>
         /// <param name="encoding"></param>
         /// <returns></returns>
-        public static string UploadFile(string url, string[] files, NameValueCollection data, Encoding encoding)
+        public static string UploadFiles(string url, string[] files, NameValueCollection data, Encoding encoding)
+        {
+            var fileKeys = new List<string>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileKeys.Add("file" + i);
+            }
+
+            return UploadFiles(url, data, files, fileKeys, "application/octet-stream", encoding);
+        }
+        #endregion
+
+        #region UploadImage
+
+        public static string UploadImage(string url, string file, NameValueCollection data)
+        {
+            var files = new[] { file };
+            var fileKeys = new List<string>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileKeys.Add("uploadimg");
+            }
+            return UploadImages(url, files, fileKeys, data, DefaultEncode);
+        }
+        public static string UploadImages(string url, string[] files, List<string> fileKeys, NameValueCollection data,
+            Encoding encoding)
+        {
+            return UploadFiles(url, data, files, fileKeys, "image/jpeg", encoding);
+        }
+
+        #endregion
+
+        ///// <summary>
+        ///// HttpUploadFile
+        ///// </summary>
+        ///// <param name="url"></param>
+        ///// <param name="files"></param>
+        ///// <param name="data"></param>
+        ///// <param name="encoding"></param>
+        ///// <returns></returns>
+        //public static string UploadFile(string url, string[] files, NameValueCollection data, Encoding encoding)
+        //{
+        //    string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+        //    byte[] boundarybytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+        //    byte[] endbytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+
+        //    //1.HttpWebRequest
+        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        //    request.ContentType = "multipart/form-data; boundary=" + boundary;
+        //    request.Method = "POST";
+        //    request.KeepAlive = true;
+        //    request.Credentials = CredentialCache.DefaultCredentials;
+
+        //    using (Stream stream = request.GetRequestStream())
+        //    {
+        //        //1.1 key/value
+        //        string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+        //        if (data != null)
+        //        {
+        //            foreach (string key in data.Keys)
+        //            {
+        //                stream.Write(boundarybytes, 0, boundarybytes.Length);
+        //                string formitem = string.Format(formdataTemplate, key, data[key]);
+        //                byte[] formitembytes = encoding.GetBytes(formitem);
+        //                stream.Write(formitembytes, 0, formitembytes.Length);
+        //            }
+        //        }
+
+        //        //1.2 file
+        //        string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: application/octet-stream\r\n\r\n";
+        //        byte[] buffer = new byte[4096];
+        //        int bytesRead = 0;
+        //        for (int i = 0; i < files.Length; i++)
+        //        {
+        //            stream.Write(boundarybytes, 0, boundarybytes.Length);
+        //            string header = string.Format(headerTemplate, "file" + i, Path.GetFileName(files[i]));
+        //            byte[] headerbytes = encoding.GetBytes(header);
+        //            stream.Write(headerbytes, 0, headerbytes.Length);
+        //            using (FileStream fileStream = new FileStream(files[i], FileMode.Open, FileAccess.Read))
+        //            {
+        //                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+        //                {
+        //                    stream.Write(buffer, 0, bytesRead);
+        //                }
+        //            }
+        //        }
+
+        //        //1.3 form end
+        //        stream.Write(endbytes, 0, endbytes.Length);
+        //    }
+        //    //2.WebResponse
+        //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        //    using (StreamReader stream = new StreamReader(response.GetResponseStream() ?? throw new InvalidOperationException()))
+        //    {
+        //        var result = stream.ReadToEnd();
+        //        var decodeResult = Unicode2String(result);
+        //        return decodeResult;
+        //    }
+        //}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url">访问地址</param>
+        /// <param name="fieldValues">数据字典</param>
+        /// <param name="files">文件列表</param>
+        /// <param name="fileKeys">"uploadimg"、"file0"</param>
+        /// <param name="fileContentType">"image/jpeg"</param>
+        /// <param name="encoding">编码，如UTF8</param>
+        /// <returns></returns>
+        public static string UploadFiles(string url, NameValueCollection fieldValues, string[] files, List<string> fileKeys, string fileContentType, Encoding encoding)
         {
             string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
             byte[] boundarybytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
             byte[] endbytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
 
             //1.HttpWebRequest
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.ContentType = "multipart/form-data; boundary=" + boundary;
+            System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            request.ContentType = $"multipart/form-data; boundary=" + boundary;
             request.Method = "POST";
             request.KeepAlive = true;
             request.Credentials = CredentialCache.DefaultCredentials;
@@ -85,25 +196,26 @@ namespace Kybs0.Net.Utils
             {
                 //1.1 key/value
                 string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
-                if (data != null)
+                if (fieldValues != null)
                 {
-                    foreach (string key in data.Keys)
+                    foreach (string key in fieldValues.Keys)
                     {
                         stream.Write(boundarybytes, 0, boundarybytes.Length);
-                        string formitem = string.Format(formdataTemplate, key, data[key]);
+                        string formitem = string.Format(formdataTemplate, key, fieldValues[key]);
                         byte[] formitembytes = encoding.GetBytes(formitem);
                         stream.Write(formitembytes, 0, formitembytes.Length);
                     }
                 }
 
                 //1.2 file
-                string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: application/octet-stream\r\n\r\n";
+                string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
                 byte[] buffer = new byte[4096];
                 int bytesRead = 0;
                 for (int i = 0; i < files.Length; i++)
                 {
+                    //
                     stream.Write(boundarybytes, 0, boundarybytes.Length);
-                    string header = string.Format(headerTemplate, "file" + i, Path.GetFileName(files[i]));
+                    string header = string.Format(headerTemplate, fileKeys[i], Path.GetFileName(files[i]), fileContentType);
                     byte[] headerbytes = encoding.GetBytes(header);
                     stream.Write(headerbytes, 0, headerbytes.Length);
                     using (FileStream fileStream = new FileStream(files[i], FileMode.Open, FileAccess.Read))
